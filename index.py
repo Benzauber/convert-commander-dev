06df7bbb7e703ac1215c83d00e53e26943f038d5
Benzauber <ben.leibundgut@gmail.com>
2024-10-22 18:09:45 +0000

    added docs pace

--- index.py
+++ index.py
@@ -93,5 +93,9 @@ def empfange_daten():
 
     return jsonify({"status": "erfolgreich empfangen", "message": "Bitte laden Sie jetzt eine Datei hoch"})
 
+@app.route('/docs')
+def doc():
+    return render_template("docs.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host="192.168.1.94")
